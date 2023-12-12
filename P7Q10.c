d8cf72a0f28afe24f829a70dd247fc52f2ad43a6
JEETU KANHAIYA PRASAD <142589448+jeetubro-158@users.noreply.github.com>
2023-12-12 09:56:34 +0530

    Update P7Q10.c

--- P7Q10.c
+++ P7Q10.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
-void swap(int*, int*);
-void main()
-{
-    int a=10,b=5;
-    printf("Before swapping a = %d & b = %d\n", a,b);
-    swap(&a, &b);
-    printf("After swapping a = %d & b = %d\n", a,b);
 
-}
-void swap(int *p1, int *p2){
-    int temp;
-    temp = *p1;
-    *p1 = *p2;
-    *p2 = temp;
-}
